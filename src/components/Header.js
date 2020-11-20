00c1b31ef06094f0c505d21f1c8bc369515895e4
Sam Fullerton <Samdfullerton@gmail.com>
2020-11-19 18:20:51 -0600

    edited app.js

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div className="headertag">
+            <h1>Directory</h1>
+        </div>
+    )
+}
+export default Header
---
+++ src/components/Search.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Search(props) {
+    return (
+        <div className="searchBar">
+            <input type="search"
+            placeholder="Search"
+
+            />
+        </div>
+    )
+}
+export default Search;
+ 
